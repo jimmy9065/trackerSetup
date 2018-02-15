30231607d59a9a8b0ce2175d139c3f7356f89363
Jimmy <jimmy9065@hotmail.com>
2018-02-15 00:49:10 -0800

    set the default output for isTrack() to be true

--- dist/bundle.js
+++ dist/bundle.js
@@ -259,7 +259,11 @@ function setupTracker(window,document,spURL,LeadURI,reportSubmitServer,appID) {
   });
     
 };
-;var isTrack = function(location) {
+;//The input of isTrack function is document.location
+//Use any methods you want to decide if this page needed to be track.
+//If this page should be tracked, then return true;
+var isTrack = function(location) {
   console.log("this is add-on js:" + location.pathname);
-  return location.pathname == '/';
+  //return location.pathname == '/';
+  return true;
 }
--- src/rule.js
+++ src/rule.js
@@ -1,4 +1,8 @@
+//The input of isTrack function is document.location
+//Use any methods you want to decide if this page needed to be track.
+//If this page should be tracked, then return true;
 var isTrack = function(location) {
   console.log("this is add-on js:" + location.pathname);
-  return location.pathname == '/';
+  //return location.pathname == '/';
+  return true;
 }
