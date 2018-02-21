180ce619ab07c55dfb7fa263f17b144b82ed2793
LYZ <jimmy9065@hotmail.com>
2018-02-20 22:40:46 -0800

    working on it

--- src/globalTracker.js
+++ src/globalTracker.js
@@ -1,21 +1,29 @@
 var addGlobalListener = function(variableName, eventType, tagId) {
   if(document.xsyGlobal[variableName] == undefined){
     let handler = {};
-    if(eventType == 'btn')
+    let e = document.getElementById(tagId);
+    if(eventType == 'link')
       handler.set = function(obj, prop, value){
-        console.log('emit btn signal for ' + )
+        let href = value;
+        if(href == undefined)
+          href = e.href;
+        let matcher = new RegExp('\.([a-zA-Z0-9]+)$');
+        let matchs = href.match(matcher);
+        let type = 'n/a'
+        if(matchs && matchs[1]){
+          type = matchs[1];
+          console.log('finded');
+          console.log(matchs);
+        }
+        window.snowplow('trackStructEvent', 'link', 'download', tagId, e.href, type);
+        console.log('emit click signal for ' + tagId + " link:" + e.href + " type:" + type);
       };
-    else if(eventType == 'video'){
+    else if(eventType == 'click'){
       handler.set = function(obj, prop, value){
-
+        
       };
     }
     else if(eventType == 'form'){
-      handler.set = function(obj, prop, value){
-
-      };
-    }
-    else if(eventType == 'unknown'){
       handler.set = function(obj, prop, value){
 
       };
